b51c99289701fd9a0a223ceb2f5dcc820eb79a49
Mahesh <maheshkhatri2058@gmail.com>
2025-10-08 11:11:41 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Footer from "./components/Footer"
+export default () => {
+  return(
+    <>
+        <h2 className="text-3xl text-orange-600 font-bold">Hello</h2>
+    <Footer/>
+
+    </>
+
+  )
+  
+}
